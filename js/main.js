c2bb7676f89e793bb2b191e074aae3d3be59f4ff
hiren95 <hirenpadariya2014@gmail.com>
2021-04-06 07:55:57 +0530

    change update

--- js/main.js
+++ js/main.js
@@ -303,7 +303,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("15 January 2021 15:56:00 GMT+01:00");			
+		var endTime = new Date("08 April 2021 15:56:00 GMT+01:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
